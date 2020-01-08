d64eaa156860eeacda4f9dda7dad01576bf4459c
Jagmeet Singh Khanuja <sunnykhanujaa@gmail.com>
2020-01-08 22:43:15 +0530

    Update JSON_to_CSV.js

--- JSON_to_CSV.js
+++ JSON_to_CSV.js
@@ -1,7 +1,7 @@
 /*
 *
 * Author: Jagmeet Singh Khanuja 
-* Mail: jagmeetsingh.work@gmail.com
+* Mail: emailjagmeetsingh@gmail.com
 * Data Created: Sept 12, 2018
 * Javascript to convert JSON Data to CSV (Excel)
 *
@@ -11,6 +11,7 @@ function getCSV() {
 		var jsonData = $("#_json").val();
 		JSONToCSVConvertor(jsonData, "UserExport", true);
 	}
+
 function ConvertToCSV(objArray) {
             var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
             var str = '';
@@ -34,29 +35,22 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
 //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
 var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
 var CSV = '';    
-//This condition will generate the Label/Header
 if (ShowLabel) {
     var row = "";
 
-    //This loop will extract the label from 1st index of on array
     for (var index in arrData[0]) {
-        //Now convert each value to string and comma-seprated
         row += index + ',';
     }
     row = row.slice(0, -1);
-    //append Label row with line break
     CSV += row + '\r\n';
 }
 
-//1st loop is to extract each row
 for (var i = 0; i < arrData.length; i++) {
     var row = "";
-    //2nd loop will extract each column and convert it in string comma-seprated
     for (var index in arrData[i]) {
         row += '"' + arrData[i][index] + '",';
     }
     row.slice(0, row.length - 1);
-    //add a line break after each row
     CSV += row + '\r\n';
 }
 
@@ -65,11 +59,10 @@ if (CSV == '') {
     return;
 }   
 
-//this trick will generate a temp "a" tag
 var link = document.createElement("a");    
 link.id="lnkDwnldLnk";
 
-//this part will append the anchor tag and remove it after automatic click
+//this will append the anchor tag and remove it after automatic click
 document.body.appendChild(link);
 
 var csv = CSV;  
